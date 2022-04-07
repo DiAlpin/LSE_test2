161753039cad35858fb88f1373f83312b764659e
DiAlpin <68875763+DiAlpin@users.noreply.github.com>
2022-04-07 22:50:48 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,3 +1,32 @@
-print('hello')
 
+from dash import Dash, dcc, html, Input, Output
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+
+    dcc.Textarea(value='insert Action1', style={'width': '100%', 'height': 20}),
+    dcc.RadioItems(['Unfinished', 'Done'], 'Unfinished'),
+
+    html.Hr(),
+    dcc.Textarea(value='insert Action2', style={'width': '100%', 'height': 20}),
+    dcc.RadioItems(['Unfinished', 'Done'], 'Unfinished'),
+
+    html.Hr(),
+    dcc.Textarea(value='insert Action3', style={'width': '100%', 'height': 20}),
+    dcc.RadioItems(['Unfinished', 'Done'], 'Unfinished'),
+
+    html.Hr(),
+    dcc.Textarea(value='insert Action4', style={'width': '100%', 'height': 20}),
+    dcc.RadioItems(['Unfinished', 'Done'], 'Unfinished'),
+
+    html.Hr(),
+    dcc.Textarea(value='insert Action5', style={'width': '100%', 'height': 20}),
+    dcc.RadioItems(['Unfinished', 'Done'], 'Unfinished'),
+
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
